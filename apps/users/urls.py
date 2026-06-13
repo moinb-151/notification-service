58311bcc56b20d632c0f1f4b482dfbe37cd710a3
Moin Bagban <moinbagban81@gmail.com>
2026-06-13 13:19:16 +0530

    added auth views

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import LoginView, RefreshView, UserRegistrationView
+from .views import AuthView, LoginView, LogoutView, RefreshView, UserRegistrationView
 
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="user-registration"),
     path("login/", LoginView.as_view(), name="user-login"),
     path("refresh/", RefreshView.as_view(), name="token-refresh"),
+    path("test/", AuthView.as_view(), name="auth-view"),
+    path("logout/", LogoutView.as_view(), name="user-logout"),
 ]
