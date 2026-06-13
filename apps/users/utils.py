75293a8811638ad102d87c3207b80e2435cf3db9
Moin Bagban <moinbagban81@gmail.com>
2026-06-13 13:19:53 +0530

    added utility function

--- apps/users/utils.py
+++ apps/users/utils.py
@@ -8,7 +8,6 @@ def _set_auth_cookie(response, key, value, max_age, path="/"):
         httponly=settings.SIMPLE_JWT.get("AUTH_COOKIE_HTTP_ONLY", True),
         secure=settings.SIMPLE_JWT.get("AUTH_COOKIE_SECURE", not settings.DEBUG),
         samesite=settings.SIMPLE_JWT.get("AUTH_COOKIE_SAMESITE", "Strict"),
-        domain=settings.SIMPLE_JWT.get("AUTH_COOKIE_DOMAIN", None),
         max_age=max_age,
         path=path,
     )
