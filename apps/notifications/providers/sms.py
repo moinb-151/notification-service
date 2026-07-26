f607364b2011c8a3c10fcacee666e81964e22932
Moin Bagban <moinbagban81@gmail.com>
2026-07-26 14:53:09 +0530

    added sms provider

--- apps/notifications/providers/sms.py
+++ apps/notifications/providers/sms.py
@@ -3,4 +3,25 @@ from django.conf import settings
 
 
 class SMSProvider:
-    pass
+    @staticmethod
+    def send(phone_number: str, message: str) -> str:
+        url = f"{settings.FAST2SMS_BASE_URL}/dev/bulkV2"
+
+        headers = {
+            "accept": "application/json",
+            "Authorization": settings.FAST2SMS_API_KEY,
+            "content-type": "application/json",
+        }
+
+        body = {
+            "route": "q",
+            "message": message,
+            "numbers": phone_number,
+        }
+
+        response = requests.post(url, json=body, headers=headers)
+
+        data = response.json()
+
+        if data.get("return", ""):
+            return data.get("request_id", "")
