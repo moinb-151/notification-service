ae24cf5e1248b04c933f9df7a2efde3ec6e79786
Moin Bagban <moinbagban81@gmail.com>
2026-09-08 20:06:14 +0530

    added health route

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from .views import health_check
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("health/", health_check),
     path("users/", include("apps.users.urls")),
     path("orders/", include("apps.orders.urls")),
     path("notifications/", include("apps.notifications.urls")),
