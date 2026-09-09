5310dc1d59059babb3a540a0fc8f9a8e40cbf3b7
Moin Bagban <moinbagban81@gmail.com>
2026-09-09 11:09:05 +0530

    added celery config to docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,28 @@
 
 set -e
 
-echo "Running database migrations..."
-python manage.py migrate --noinput
+case "$1" in
+    web)
+        echo "Running database migrations..."
+        python manage.py migrate --noinput
 
-echo "Starting Uvicorn..."
-exec uvicorn config.asgi:application \
-    --host 0.0.0.0 \
-    --port 8000
+        echo "Starting Uvicorn..."
+        exec uvicorn config.asgi:application \
+            --host 0.0.0.0 \
+            --port 8000
+        ;;
+
+    worker)
+        echo "Starting Celery worker..."
+        exec celery -A config worker --loglevel=info
+        ;;
+
+    beat)
+        echo "Starting Celery Beat..."
+        exec celery -A config beat --loglevel=info
+        ;;
+
+    *)
+        exec "$@"
+        ;;
+esac
